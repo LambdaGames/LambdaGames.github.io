7a79afdd6123c7b61c014c9fcd4d933bca3a30f5
Guillaume <lambdagamescontactme@gmail.com>
2021-02-26 03:02:41 +0100

    test captcha

--- captcha.php
+++ captcha.php
@@ -2,6 +2,13 @@
 
 session_start();
 
+if (isset($_POST['code'])) {
+    if ($_POST['code'] == $_SESSION['code'])
+    echo "captcha valide !";
+    else
+    echo "captcha invalide...";
+}
+
 $code = rand(1000,9999);
 $_SESSION['code'] = $code;
 
@@ -16,4 +23,6 @@ imagestring($img, $font, 5,5, $code, $textcolor);
 imagettftext($img, 23, 0, 3, 30, $textcolor, $font, $_SESSION['code']);
 imagejpeg($img);
 imagedestroy($img);
+
+
 ?>
